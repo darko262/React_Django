b19c0940f775723dadad40fe2de7fd30476c2913
darko262 <s.padilla01@ufromail.cl>
2023-05-19 23:30:18 -0400

    cambios en core

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+import os
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mf3wo*j%x#!el*@!g1yrf^e$%6ymqkao(h^@dyhk%x*c&ytggq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =env.list('ALLOWED_HOST_DEV')
 
 
 # Application definition
@@ -111,3 +115,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+if not DEBUG:
+    ALLOWED_HOSTS=env.list('ALLOWED_HOST_DEPLOY')
