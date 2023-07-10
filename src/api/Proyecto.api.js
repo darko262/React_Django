79722e2678d55026614117e88db534df28cb4c1c
darko262 <s.padilla01@ufromail.cl>
2023-07-10 19:24:27 -0400

    cambiando direccion backend

--- src/api/Proyecto.api.js
+++ src/api/Proyecto.api.js
@@ -2,13 +2,15 @@ import axios from 'axios'
 
 export const getAllProyectos = () =>{
     // return axios.get('http://localhost:8000/api/blog/list') //estp sirve para el react el otro pra el python
-    return axios.get('http://127.0.0.1:8000/api/blog/list')
+    // return axios.get('http://127.0.0.1:8000/api/blog/list')
+    return axios.get('http://padillacode.pythonanywhere.com/api/blog/list')
     
 
 }
 export const getProyecto = (slug) =>{
     const parametro = slug.slug
-    return axios.get(`http://127.0.0.1:8000/api/blog/detail/${parametro}`)
+    return axios.get(`http://padillacode.pythonanywhere.com/api/blog/detail/${parametro}`)
+    //return axios.get(`http://127.0.0.1:8000/api/blog/detail/${parametro}`)
     // return axios.get(`http://localhost:8000/api/blog/detail/${parametro}`)
     
 
