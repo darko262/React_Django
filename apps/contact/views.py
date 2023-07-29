40642a421965afea43e4b52593a4b729c0e4884b
darko262 <s.padilla01@ufromail.cl>
2023-07-29 01:17:10 -0400

    mejoramiento casi final5

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -7,6 +7,7 @@ import requests
 import os
 from .models import Contact
 from django.core.mail import send_mail, EmailMessage
+from serializers import *
 
 
 from django.conf import settings
@@ -18,53 +19,56 @@ from django.conf import settings
 class ContactCreateView(APIView):
     permission_classes = (permissions.AllowAny,)
     @csrf_protect
-    def post(self, request, format=None):
-        data = self.request.data
+    def post(self, request):
+            serializers = ContactSerializer(data=request.data)
+            if serializers.is_valid(raise_exception=True):
+                   serializers.save()
+    # def post(self, request, format=None):
+    #     data = self.request.data
 
-        name = data.POST.get('name','')
-        email = data.POST.get('email','')
-        subject = data.POST.get('subject','')
-        message = data.POST.get('message','')
-        phone = data.POST.get('phone','')
-        # email = data['email']
-        # subject = data['subject']
-        # message = data['message']
-        # phone = data['phone']
+    #     name = data.POST.get('name','')
+    #     email = data.POST.get('email','')
+    #     subject = data.POST.get('subject','')
+    #     message = data.POST.get('message','')
+    #     phone = data.POST.get('phone','')
+    #     # email = data['email']
+    #     # subject = data['subject']
+    #     # message = data['message']
+    #     # phone = data['phone']
         
-        email = EmailMessage(
-            "envio el siguiente email"
-            "De {} {} \n\n {} {} {} ".format(name,email,subject,message,phone),
-            "padillaseba06@gmail.com",
-            ["seba.padilla@live.cl"],
-            reply_to=[email]
+    #     email = EmailMessage(
+    #         "envio el siguiente email"
+    #         "De {} {} \n\n {} {} {} ".format(name,email,subject,message,phone),
+    #         "padillaseba06@gmail.com",
+    #         ["seba.padilla@live.cl"],
+    #         reply_to=[email]
 
-        )
-        try:
-            email.send()
-            # send_mail(
-            #     subject, 
-            #     'New Client Request: '
-            #     + '\n\nName: ' + name 
-            #     + '\nEmail: ' + email
-            #     + '\n\nMessage:\n' + message
-            #     + '\nPhone: ' + phone,
-            #     'padillaseba06@gmail.com',
-            #     ['seba.padilla@live.cl'],
-            #     fail_silently=False
-            # )
+    #     )
+    #     try:
+    #         email.send()
+    #         # send_mail(
+    #         #     subject, 
+    #         #     'New Client Request: '
+    #         #     + '\n\nName: ' + name 
+    #         #     + '\nEmail: ' + email
+    #         #     + '\n\nMessage:\n' + message
+    #         #     + '\nPhone: ' + phone,
+    #         #     'padillaseba06@gmail.com',
+    #         #     ['seba.padilla@live.cl'],
+    #         #     fail_silently=False
+    #         # )
 
-            Contact.objects.create(
-                name=name,
-                email=email,
-                phone=phone,
-                subject=subject,
-                message=message,
+    #         Contact.objects.create(
+    #             name=name,
+    #             email=email,
+    #             phone=phone,
+    #             subject=subject,
+    #             message=message,
                 
-            )
+    #         )
             
-            return Response({'success': 'Message sent successfully'})
-        except:
-            return Response({'error': 'Message not sent'})
+            return Response(serializers.data)
+
 
 
 # email_host_user = os.environ.get('EMAIL_HOST_USER')
