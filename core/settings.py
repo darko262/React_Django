828a2619dbc3de392c05f259affc185b9a7f918b
darko262 <s.padilla01@ufromail.cl>
2023-08-04 13:35:15 -0400

    mejoramiento stting

--- core/settings.py
+++ core/settings.py
@@ -169,12 +169,13 @@ CORS_ALLOWED_ORIGINS = ['http://padillacode.pythonanywhere.com', 'https://padill
 
 
 
-EMAIL_HOST= config('EMAIL_HOST')
-EMAIL_PORT=config('EMAIL_PORT')
-EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='padillaseba06@gmail.com'
+EMAIL_USE_TLS= True
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_PASSWORD='bonrvsmlewduuoua'
-EMAIL_USE_TLS= config('EMAIL_USE_TLS')
+
+
     
 
     
