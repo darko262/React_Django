63afcdb3851d8627aa4fb41428fb709bc89d9562
darko262 <s.padilla01@ufromail.cl>
2023-08-10 12:59:20 -0400

    mejoramiento url

--- core/urls.py
+++ core/urls.py
@@ -3,13 +3,16 @@ from django.urls import path, re_path,include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+import environ
+env = environ.Env()
+ADMIN_URL_PATH = env('ADMIN_URL_PATH', default='admin/')
 
 urlpatterns = [
     path('api/blog/', include('apps.blog.urls')),
     path('api/category/', include('apps.category.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api/contact/', include('apps.contact.urls')),
-    path('admin/', admin.site.urls),
+    path(ADMIN_URL_PATH, admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
